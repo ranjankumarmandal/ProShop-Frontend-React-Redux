f7a37ebe218178ad9809bea5ab9b73d3ce8413b8
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-30 10:54:03 +0530

    completed Header.js component with react-bootstrap lib and bootswatch bootstrap.min.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -6,8 +7,10 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <p>App component</p>
+      <main className='py-3'>
+        <Container>
+          <p>App component</p>
+        </Container>
       </main>
       <Footer />
     </>
