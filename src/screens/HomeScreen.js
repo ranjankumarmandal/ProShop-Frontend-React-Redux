0a8c3585cc5e47f80677084ce909469163e5922e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-06 00:40:15 +0530

    added react-redux hooks useDispatch and useSelector to HomeScreen and recieved action method listProducts() and reducers root state productList as stored in store and implemented that in this local component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,23 +6,29 @@ import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  const products = [];
-
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
