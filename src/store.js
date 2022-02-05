eac0b86683c5df8317b1897884387e8784ccd214
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-05 21:59:57 +0530

    added Provider component from react-redux with store into main index.js root component and wrapped app.js component inside it

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware([...middleware]))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
