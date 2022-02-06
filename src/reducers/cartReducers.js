8873764c5740143108ec4515326bc1c484047c11
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-06 16:52:07 +0530

    completed cartReducers.js

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,18 +1,22 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
-export const cartReducer = (state = { cartItem: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-      const existItem = state.cartItem.find((x) => x.product === item.product);
+      const existItem = state.cartItems.find((x) => x.product === item.product);
       if (existItem) {
         return {
           ...state,
-          cartItem: state.cartItem.map((x) =>
+          cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
       } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
       }
 
     default:
