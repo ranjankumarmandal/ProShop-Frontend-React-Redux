c44f0a5a5aea686850a1a311ae40cc0d3ca51640
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-06 14:08:44 +0530

    added qty functionality with drop down, adde history lin on click of Add To Cart button and updated component PageNotFoundScreen.js

--- src/screens/PageNotFoundScreen.js
+++ src/screens/PageNotFoundScreen.js
@@ -4,6 +4,15 @@ const PageNotFoundScreen = () => {
   return (
     <div>
       <h1>Page Doesn't Exist!!</h1>
+      <p>
+        Having issue, please connect with the owner -{' '}
+        <a
+          href='https://www.linkedin.com/in/ranjan-kumar-m-818367158/'
+          target='_blank'
+        >
+          Ranjan Kumar Mandal
+        </a>
+      </p>
     </div>
   );
 };
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -15,7 +15,7 @@ import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
   const [qty, setQty] = useState(0);
 
   const dispatch = useDispatch();
@@ -25,6 +25,10 @@ const ProductScreen = ({ match }) => {
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, []);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
 
   return (
     <>
@@ -100,6 +104,7 @@ const ProductScreen = ({ match }) => {
 
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className='btn-block'
                     type='button'
                     disabled={product.countInStock === 0}
