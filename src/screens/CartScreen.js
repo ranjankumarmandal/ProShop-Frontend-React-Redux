e1e65bfa1cc499ec21b6c7c6317e2c475c935934
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-07 08:42:25 +0530

    added useDispatch() and useSelector() hook from react-redux and dispatching action addToCart to reducer in component CaartScreen.js

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -15,8 +15,13 @@ import { addToCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
-  const qty = location.search ? location.search.split('=')[1] : 1;
-  console.log(qty);
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) dispatch(addToCart(productId, qty));
+  }, []);
 
   return <div>Cart Page</div>;
 };
