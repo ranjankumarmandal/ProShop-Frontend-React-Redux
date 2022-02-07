03530a0c8d5b7da99a306a2411dbdc7cbc1c4fdb
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-07 07:49:37 +0530

    started working on the functionality for component CartScreen.js

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  const qty = location.search ? location.search.split('=')[1] : 1;
+  console.log(qty);
+
   return <div>Cart Page</div>;
 };
 
