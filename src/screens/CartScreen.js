eb883c7f32c9a81daa28ff5aba6e5c6187c2c4ad
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-10 05:52:52 +0530

    added a cart heading and a link to return to home page in component CartScreen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -25,7 +25,22 @@ const CartScreen = ({ match, location, history }) => {
     if (productId) dispatch(addToCart(productId, qty));
   }, [dispatch, productId, qty]);
 
-  return <div>Cart Page</div>;
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>{' '}
+          </Message>
+        ) : (
+          <ListGroup variant='flush'></ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
