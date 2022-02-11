e9b720809586143ad2af5667ccd3c4e64f2c310c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-11 23:40:58 +0530

    add a card in second column of grid in component CartScree

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -81,7 +81,9 @@ const CartScreen = ({ match, location, history }) => {
           </ListGroup>
         )}
       </Col>
-      <Col md={4}></Col>
+      <Col md={4}>
+        <Card></Card>
+      </Col>
     </Row>
   );
 };
