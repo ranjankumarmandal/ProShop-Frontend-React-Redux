d1d9e9f22468b69912cd065a297cd500ac463355
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-13 21:31:10 +0530

    added redirect on checkoutHandler() in component CartScreen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -30,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
   };
 
   const checkoutHandler = () => {
-    console.log('checkout');
+    history.push('/login?redirect=shipping');
   };
 
   return (
