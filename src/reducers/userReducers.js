be03925cbf6970ceb12f3454f5084fa1e05c97ff
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-21 00:31:47 +0530

    if login success return loading false and user information datas

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -8,5 +8,7 @@ export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
       return { loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
   }
 };
