a0d099988e630fdef09bc6a720a0843798420e97
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-21 08:32:59 +0530

    created submit button to the form

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -34,6 +34,10 @@ const LoginScreen = () => {
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          Sign In
+        </Button>
       </Form>
     </FormContainer>
   );
