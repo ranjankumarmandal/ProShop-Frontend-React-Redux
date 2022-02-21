8022d8e83b4d6aaf8355190ab88f856fc8d09df0
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-21 08:53:47 +0530

    access the userLogin states from store with userSelector

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,6 +13,8 @@ const LoginScreen = ({ location }) => {
 
   const dispatch = useDispatch();
 
+  const userLogin = useSelector((state) => state.userLogin);
+
   const redirect = location.search ? location.search.split('=')[1] : '/';
 
   const submitHandler = (e) => {
