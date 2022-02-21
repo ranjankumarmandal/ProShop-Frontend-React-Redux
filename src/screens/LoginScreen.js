84af9d0eaf7baa56a83b039f53e16e33b1be547f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-21 08:36:14 +0530

    added link for register in parent component LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -39,6 +39,15 @@ const LoginScreen = () => {
           Sign In
         </Button>
       </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
     </FormContainer>
   );
 };
