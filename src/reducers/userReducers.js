a7d4bbd768d2330ab464729d34d21133486574b3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 21:39:34 +0530

    completed userUpdateProfileReducer

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -56,13 +56,13 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
   }
 };
 
-export const userUpdateProfileReducer = (state = { user: {} }, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
   switch (action.type) {
-    case USER_DETAILS_REQUEST:
-      return { ...state, loading: true };
-    case USER_DETAILS_SUCCESS:
-      return { loading: false, user: action.payload };
-    case USER_DETAILS_FAIL:
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+    case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
