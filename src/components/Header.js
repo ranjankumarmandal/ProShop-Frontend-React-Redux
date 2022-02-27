522628d5a4eadd86e58f5bdd764a2367c4be48a0
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 11:04:53 +0530

    added username as title in navdropdown if user logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown>
+                <NavDropdown title={userInfo.name} id='username'>
                   <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
