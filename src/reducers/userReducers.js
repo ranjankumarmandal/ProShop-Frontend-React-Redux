4ec7628fc14dc999182b7108eaf32f67345b7160
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 18:54:17 +0530

    completed reducer userDetailsReducer

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -39,13 +39,13 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 };
 
-export const userDetailsReducer = (state = {}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
-    case USER_REGISTER_REQUEST:
-      return { loading: true };
-    case USER_REGISTER_SUCCESS:
-      return { loading: false, userInfo: action.payload };
-    case USER_REGISTER_FAIL:
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
