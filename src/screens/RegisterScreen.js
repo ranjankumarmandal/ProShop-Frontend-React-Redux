f3b8c2c62fd94bd0ee97491783d93ec8ce87ce8a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 13:14:41 +0530

    import action register to component RegisterScreen.js

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -4,7 +4,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { login } from '../actions/userActions';
+import { register } from '../actions/userActions';
 import FormContainer from '../components/FormContainer';
 
 const RegisterScreen = ({ location, history }) => {
