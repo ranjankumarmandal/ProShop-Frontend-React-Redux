541ae0794d623145b3327181f3c4bf432dd3a811
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 19:12:14 +0530

    started working on action getUserDetails() in userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -88,3 +88,43 @@ export const register = (name, email, password) => async (dispatch) => {
     });
   }
 };
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      'http://localhost:5000/api/users',
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
