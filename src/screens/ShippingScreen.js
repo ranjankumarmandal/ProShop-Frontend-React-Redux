bc3d2293f900eeee85140c882649d770965cb09b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-28 09:22:23 +0530

    done with all the state variables for component ShippingScreen.js

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -3,7 +3,12 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 
-const ShippingScreen = () => {
+const ShippingScreen = ({ history }) => {
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+
   return <div>ShippingScreen</div>;
 };
 
