1c48b14547083e2a08ef03229759064d6e3bab07
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-02 00:10:05 +0530

    added conditional rendering in component CheckoutSteps.js

--- src/components/CheckoutSteps.js
+++ src/components/CheckoutSteps.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const CheckoutSteps = () => {
-  return <div>CheckoutSteps</div>;
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className='justify-content-center mb-4'>
+      <Nav.Item>
+        {step1 ? (
+          <LinkContainer to='/login'>
+            <Nav.Link>Sign In</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
 };
 
 export default CheckoutSteps;
