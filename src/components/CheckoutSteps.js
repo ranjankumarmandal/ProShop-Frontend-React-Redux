36e08d4d270ed35b6ce98589a845909fe5b17b3d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-01 23:51:37 +0530

    imports done

---
+++ src/components/CheckoutSteps.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const CheckoutSteps = () => {
+  return <div>CheckoutSteps</div>;
+};
+
+export default CheckoutSteps;
