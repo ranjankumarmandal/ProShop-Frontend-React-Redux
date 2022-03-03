8f4e63b24077b23a61a4a68baea17871eafd6f69
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-03 09:06:02 +0530

    added route /payment to App.js for component PaymentScreen.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
             <Route exact path='/profile' component={ProfileScreen} />
             <Route exact path='/cart/:id?' component={CartScreen} />
             <Route exact path='/shipping' component={ShippingScreen} />
+            <Route exact path='/payment' component={PaymentScreen} />
             <Route component={PageNotFoundScreen} />
           </Switch>
         </Container>
