a908bf2c7e760d94d55eaf4fdc24949b97e1bd3c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-03 09:11:30 +0530

    completed component PaymentScreen.js

--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -28,29 +28,29 @@ const PaymentScreen = ({ history }) => {
       <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as='legend'>Select Method</Form.Label>
+
+          <Col>
+            <Form.Check
+              type='radio'
+              label='PayPal or Credit Card'
+              id='PayPal'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+
+            <Form.Check
+              type='radio'
+              label='Stripe'
+              id='Stripe'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
         </Form.Group>
-
-        <Col>
-          <Form.Check
-            type='radio'
-            label='PayPal or Credit Card'
-            id='PayPal'
-            name='paymentMethod'
-            value='PayPal'
-            checked
-            onChange={(e) => setPaymentMethod(e.target.value)}
-          ></Form.Check>
-
-          <Form.Check
-            type='radio'
-            label='Stripe'
-            id='Stripe'
-            name='paymentMethod'
-            value='Stripe'
-            checked
-            onChange={(e) => setPaymentMethod(e.target.value)}
-          ></Form.Check>
-        </Col>
+        <br />
 
         <Button type='submit' variant='primary'>
           Continue
