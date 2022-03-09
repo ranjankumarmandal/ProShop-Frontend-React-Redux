c1d31a5f93251b0ee0f17d81492f3fd7900c6a08
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-09 18:26:30 +0530

    completed implementation for cart.totalPrice in component PlaceOrderScreeen.js\

--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -14,9 +14,13 @@ const PlaceOrderScreen = () => {
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, curr) => acc + curr.price * curr.qty, 0)
   );
-
   cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
 
   const placeOrderHandler = () => {};
 
