7572cacf0acb598e48d8d6765a2ccd5fca198b35
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-12 12:58:18 +0530

    import orderCreateReducer at store.js

--- src/store.js
+++ src/store.js
@@ -12,6 +12,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
