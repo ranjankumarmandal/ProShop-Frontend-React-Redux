3fc2facf366e3dd38a1342cc28e77e11dfb33bdf
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-12 19:39:32 +0530

    added dispatch and got state values using getState() in orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -7,4 +7,39 @@ import {
 // create order actions
 export const createOrder = (order) => (dispatch, getState) => {
   // implement create order action
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      'http://localhost:5000/api/users/profile',
+      user,
+      config
+    );
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
