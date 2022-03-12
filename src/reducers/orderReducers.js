a245b56e133aa4216453986dd431d2f2f36fccad
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-12 08:18:59 +0530

    started working on orderCreateReducer() in file orderReducers.js

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -3,3 +3,7 @@ import {
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
 } from '../constants/orderConstants';
+
+export const orderCreateReducer = (state = {}, action) => {
+  // define reducers with switch
+};
