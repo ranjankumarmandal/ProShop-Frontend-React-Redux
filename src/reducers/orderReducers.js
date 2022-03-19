3153f37ba72c0b364839e8b87f76f8bd2c651934
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-20 01:07:07 +0530

    started working on orderPayReducer in file orderReducers.js

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -65,3 +65,29 @@ export const orderDetailsReducer = (
       return state;
   }
 };
+
+export const orderPayReducer = (state = {}, action) => {
+  // define reducers with switch
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload,
+      };
+
+    case ORDER_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
