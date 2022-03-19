340bc78c00831dfae6717a16e31f1bbf4535567b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-19 10:57:03 +0530

    started working on orderDetailsReducer() in file orerReducers.js

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -32,3 +32,29 @@ export const orderCreateReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const orderDetailsReducer = (state = {}, action) => {
+  // define reducers with switch
+  switch (action.type) {
+    case ORDER_CREATE_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ORDER_CREATE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        order: action.payload,
+      };
+
+    case ORDER_CREATE_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
