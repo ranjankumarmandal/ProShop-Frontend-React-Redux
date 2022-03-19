149443d3355c88cfc525a8a700354ee488cea1bb
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-19 18:02:45 +0530

    added orderDetailsReducer in file store.js

--- src/store.js
+++ src/store.js
@@ -26,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
