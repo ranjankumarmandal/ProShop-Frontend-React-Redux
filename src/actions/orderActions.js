18f7f259b350c38479c622300c3d5f2482658668
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-19 18:06:13 +0530

    started working on action function getOrderDetails() in fle orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -44,3 +44,42 @@ export const createOrder = (order) => async (dispatch, getState) => {
     });
   }
 };
+
+export const getOrderDetails = (order) => async (dispatch, getState) => {
+  // implement createOrder() action function
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(
+      'http://localhost:5000/api/orders',
+      order,
+      config
+    );
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
