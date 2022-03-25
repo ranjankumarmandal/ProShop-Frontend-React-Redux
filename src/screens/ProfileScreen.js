71dac2913f9480531a6c158fcc1132e358c03f00
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-26 00:15:03 +0530

    add a table header with row contents id, date, total, paid and delivered

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -110,7 +110,17 @@ const ProfileScreen = ({ location, history }) => {
         ) : error ? (
           <Message variant='danger'>{errorOrders}</Message>
         ) : (
-          <Table striped bordered hover responsive className='table-sm'></Table>
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+              </tr>
+            </thead>
+          </Table>
         )}
       </Col>
     </Row>
